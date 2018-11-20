2759282472b20c69f3c3103860712e19974ba593
deepankorantak@gmail.com <deepankorantak@gmail.com>
2018-11-20 09:35:57 +0000

    initial checkin

---
+++ TestJava/SampleCode.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Deepa Korantak
+ *
+ */
+public class SampleCode {
+
+	public void testConditionNotUsed (){
+		
+		boolean a = false,b = true;		
+		int index = 0;
+		
+		if (a) { // Non-compliant
+		  index ++; // never executed
+		}
+
+		if (!a || b) { // Non-compliant; "!a" is always "true", "b" is never evaluated
+		  index = 0;
+		} else {
+		  index ++; // never executed
+		}
+	}
+
+
+}
