b3fd189384bb9bec6fb43ad5a8b9bf0f37222a65
deepankorantak@gmail.com <deepankorantak@gmail.com>
2018-11-21 07:05:45 +0000

    new file - utf-8

---
+++ TestJava/SampleCodeUtf.java
@@ -0,0 +1,19 @@
+﻿public class SampleCode {
+
+	public void testConditionNotUsed (){
+
+		boolean a = false,b = true;
+		int index = 0;
+
+		if (a) {
+		  index ++;
+		}
+
+		if (!a || b) {
+		  index --;
+		} else {
+		  index ++;
+		}
+	}
+
+}
